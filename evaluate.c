ae72ecb3413d1aeda2556a6290987711c75d7af8
Michi <mbaueretz@googlemail.com>
2020-06-06 05:19:52 +0200

    implement piece-square tables

--- evaluate.c
+++ evaluate.c
@@ -1,42 +1,275 @@
 #include "torres.h"
 
-int evaluate(Position* position)
+// Piece-square tables by Tomasz Michniewski.
+// See: https://www.chessprogramming.org/Simplified_Evaluation_Function
+
+int black_bishop_table[64] =
 {
-    int score = 0;
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10,   5,   0,   0,   0,   0,   5, -10,
+    -10,  10,  10,  10,  10,  10,  10, -10,
+    -10,   0,  10,  10,  10,  10,   0, -10,
+    -10,   5,   5,  10,  10,   5,   5, -10,
+    -10,   0,   5,  10,  10,   5,   0, -10,
+    -10,   0,   0,   0,   0,   0,   0, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20
+};
+
+int black_king_endgame_table[64] =
+{
+    -50, -30, -30, -30, -30, -30, -30, -50,
+    -30, -30,   0,   0,   0,   0, -30, -30,
+    -30, -10,  20,  30,  30,  20, -10, -30,
+    -30, -10,  30,  40,  40,  30, -10, -30,
+    -30, -10,  30,  40,  40,  30, -10, -30,
+    -30, -10,  20,  30,  30,  20, -10, -30,
+    -30, -20, -10,   0,   0, -10, -20, -30,
+    -50, -40, -30, -20, -20, -30, -40, -50
+};
+
+int black_king_table[64] =
+{
+     20,  30,  10,   0,   0,  10,  30,  20,
+     20,  20,   0,   0,   0,   0,  20,  20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30
+};
+
+int black_knight_table[64] =
+{
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20,   0,   5,   5,   0, -20, -40,
+    -30,   5,  10,  15,  15,  10,   5, -30,
+    -30,   0,  15,  20,  20,  15,   0, -30,
+    -30,   5,  15,  20,  20,  15,   5, -30,
+    -30,   0,  10,  15,  15,  10,   0, -30,
+    -40, -20,   0,   0,   0,   0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50
+};
+
+int black_pawn_table[64] =
+{
+      0,   0,   0,   0,   0,   0,   0,   0,
+      5,  10,  10, -20, -20,  10,  10,   5,
+      5,  -5, -10,   0,   0, -10,  -5,   5,
+      0,   0,   0,  20,  20,   0,   0,   0,
+      5,   5,  10,  25,  25,  10,   5,   5,
+     10,  10,  20,  30,  30,  20,  10,  10,
+     50,  50,  50,  50,  50,  50,  50,  50,
+      0,   0,   0,   0,   0,   0,   0,   0
+};
+
+int black_queen_table[64] =
+{
+    -20, -10, -10,  -5,  -5, -10, -10, -20,
+    -10,   0,   5,   0,   0,   0,   0, -10,
+    -10,   5,   5,   5,   5,   5,   0, -10,
+      0,   0,   5,   5,   5,   5,   0,  -5,
+     -5,   0,   5,   5,   5,   5,   0,  -5,
+    -10,   0,   5,   5,   5,   5,   0, -10,
+    -10,   0,   0,   0,   0,   0,   0, -10,
+    -20, -10, -10,  -5,  -5, -10, -10, -20
+};
+
+int black_rook_table[64] =
+{
+      0,   0,   0,   5,   5,   0,   0,   0,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+      5,  10,  10,  10,  10,  10,  10,   5,
+      0,   0,   0,   0,   0,   0,   0,   0
+};
+
+int white_bishop_table[64] =
+{
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10,   0,   0,   0,   0,   0,   0, -10,
+    -10,   0,   5,  10,  10,   5,   0, -10,
+    -10,   5,   5,  10,  10,   5,   5, -10,
+    -10,   0,  10,  10,  10,  10,   0, -10,
+    -10,  10,  10,  10,  10,  10,  10, -10,
+    -10,   5,   0,   0,   0,   0,   5, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20
+};
+
+int white_king_endgame_table[64] =
+{
+    -50, -40, -30, -20, -20, -30, -40, -50,
+    -30, -20, -10,   0,   0, -10, -20, -30,
+    -30, -10,  20,  30,  30,  20, -10, -30,
+    -30, -10,  30,  40,  40,  30, -10, -30,
+    -30, -10,  30,  40,  40,  30, -10, -30,
+    -30, -10,  20,  30,  30,  20, -10, -30,
+    -30, -30,   0,   0,   0,   0, -30, -30,
+    -50, -30, -30, -30, -30, -30, -30, -50
+};
+
+int white_king_table[64] =
+{
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+     20,  20,   0,   0,   0,   0,  20,  20,
+     20,  30,  10,   0,   0,  10,  30,  20
+};
+
+int white_knight_table[64] =
+{
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20,   0,   0,   0,   0, -20, -40,
+    -30,   0,  10,  15,  15,  10,   0, -30,
+    -30,   5,  15,  20,  20,  15,   5, -30,
+    -30,   0,  15,  20,  20,  15,   0, -30,
+    -30,   5,  10,  15,  15,  10,   5, -30,
+    -40, -20,   0,   5,   5,   0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50
+};
+
+int white_pawn_table[64] =
+{
+      0,   0,   0,   0,   0,   0,   0,   0,
+     50,  50,  50,  50,  50,  50,  50,  50,
+     10,  10,  20,  30,  30,  20,  10,  10,
+      5,   5,  10,  25,  25,  10,   5,   5,
+      0,   0,   0,  20,  20,   0,   0,   0,
+      5,  -5, -10,   0,   0, -10,  -5,   5,
+      5,  10,  10, -20, -20,  10,  10,   5,
+      0,   0,   0,   0,   0,   0,   0,   0
+};
+
+int white_queen_table[64] =
+{
+    -20, -10, -10,  -5,  -5, -10, -10, -20,
+    -10,   0,   0,   0,   0,   0,   0, -10,
+    -10,   0,   5,   5,   5,   5,   0, -10,
+     -5,   0,   5,   5,   5,   5,   0,  -5,
+      0,   0,   5,   5,   5,   5,   0,  -5,
+    -10,   5,   5,   5,   5,   5,   0, -10,
+    -10,   0,   5,   0,   0,   0,   0, -10,
+    -20, -10, -10,  -5,  -5, -10, -10, -20
+};
+
+int white_rook_table[64] =
+{
+      0,   0,   0,   0,   0,   0,   0,   0,
+      5,  10,  10,  10,  10,  10,  10,   5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+     -5,   0,   0,   0,   0,   0,   0,  -5,
+      0,   0,   0,   5,   5,   0,   0,   0
+};
+
+int endgame(Position* position)
+{
+    int white_queens = 0;
+    int black_queens = 0;
+    int white_rooks = 0;
+    int black_rooks = 0;
+    int white_minor_pieces = 0;
+    int black_minor_pieces = 0;
     for (int i = 0; i < 64; i++)
     {
         int square = long_index[i];
         switch (position->board[square])
         {
         case WHITE_QUEEN:
-            score += 900;
-            break;
-        case WHITE_BISHOP:
-            score += 300;
-            break;
-        case WHITE_KNIGHT:
-            score += 300;
+            white_queens++;
             break;
         case WHITE_ROOK:
-            score += 500;
+            white_rooks++;
             break;
-        case WHITE_PAWN:
-            score += 100;
+        case WHITE_BISHOP:
+        case WHITE_KNIGHT:
+            white_minor_pieces++;
             break;
         case BLACK_QUEEN:
-            score -= 900;
-            break;
-        case BLACK_BISHOP:
-            score -= 300;
-            break;
-        case BLACK_KNIGHT:
-            score -= 300;
+            black_queens++;
             break;
         case BLACK_ROOK:
-            score -= 500;
+            black_rooks++;
+            break;
+        case BLACK_BISHOP:
+        case BLACK_KNIGHT:
+            black_minor_pieces++;
+        }
+    }
+    int white_endgame = white_queens == 0
+        || (white_queens == 1 && white_rooks == 0 && white_minor_pieces <= 1);
+    int black_endgame = black_queens == 0
+        || (black_queens == 1 && black_rooks == 0 && black_minor_pieces <= 1);
+    return white_endgame && black_endgame;
+}
+
+int evaluate(Position* position)
+{
+    int score = 0;
+    int is_endgame = endgame(position);
+    for (int i = 0; i < 64; i++)
+    {
+        int square = long_index[i];
+        switch (position->board[square])
+        {
+        case WHITE_KING:
+            if (is_endgame)
+            {
+                score += white_king_endgame_table[i];
+            }
+            else
+            {
+                score += white_king_table[i];
+            }
+            break;
+        case WHITE_QUEEN:
+            score += 900 + white_queen_table[i];
+            break;
+        case WHITE_BISHOP:
+            score += 330 + white_bishop_table[i];
+            break;
+        case WHITE_KNIGHT:
+            score += 320 + white_knight_table[i];
+            break;
+        case WHITE_ROOK:
+            score += 500 + white_rook_table[i];
+            break;
+        case WHITE_PAWN:
+            score += 100 + white_pawn_table[i];
+            break;
+        case BLACK_KING:
+            if (is_endgame)
+            {
+                score -= black_king_endgame_table[i];
+            }
+            else
+            {
+                score -= black_king_table[i];
+            }
+            break;
+        case BLACK_QUEEN:
+            score -= 900 + black_queen_table[i];
+            break;
+        case BLACK_BISHOP:
+            score -= 330 + black_bishop_table[i];
+            break;
+        case BLACK_KNIGHT:
+            score -= 320 + black_knight_table[i];
+            break;
+        case BLACK_ROOK:
+            score -= 500 + black_rook_table[i];
             break;
         case BLACK_PAWN:
-            score -= 100;
+            score -= 100 + black_pawn_table[i];
         }
     }
     return position->player * score;
--- torres.h
+++ torres.h
@@ -34,6 +34,41 @@ Displacements for bishop moves.
 extern int bishop_displacements[];
 
 /*
+Piece-square table for the black bishop.
+*/
+extern int black_bishop_table[];
+
+/*
+Endgame piece-square table for the black king.
+*/
+extern int black_king_endgame_table[];
+
+/*
+Piece-square table for the black king.
+*/
+extern int black_king_table[];
+
+/*
+Piece-square table for the black knight.
+*/
+extern int black_knight_table[];
+
+/*
+Piece-square table for the black pawn.
+*/
+extern int black_pawn_table[];
+
+/*
+Piece-square table for the black queen.
+*/
+extern int black_queen_table[];
+
+/*
+Piece-square table for the black rook.
+*/
+extern int black_rook_table[];
+
+/*
 Displacements for king moves.
 */
 extern int king_displacements[];
@@ -62,6 +97,41 @@ extern int queen_displacements[];
 Displacements for rook moves.
 */
 extern int rook_displacements[];
+
+/*
+Piece-square table for the white bishop.
+*/
+extern int white_bishop_table[];
+
+/*
+Piece-square table for the white king.
+*/
+extern int white_king_table[];
+
+/*
+Endgame piece-square table for the white king.
+*/
+extern int white_king_endgame_table[];
+
+/*
+Piece-square table for the white knight.
+*/
+extern int white_knight_table[];
+
+/*
+Piece-square table for the white pawn.
+*/
+extern int white_pawn_table[];
+
+/*
+Piece-square table for the white queen.
+*/
+extern int white_queen_table[];
+
+/*
+Piece-square table for the white rook.
+*/
+extern int white_rook_table[];
 
 /*
 Adds a move to a move list.
@@ -251,6 +321,16 @@ Parameters:
 Returns: 1 if the square is empty, 0 otherwise.
 */
 int empty(Position* position, int square);
+
+/*
+Checks if a position is an endgame.
+
+Parameters:
+- Position* position: The position to be examined.
+
+Returns: 1 if it is an endgame, 0 otherwise.
+*/
+int endgame(Position* position);
 
 /*
 Statically evaluates a position.
